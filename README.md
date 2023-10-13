ba8275728a2ca786e71d2e03ed70e06169fceb48
= <=>
2023-10-13 10:16:02 -0700

    feat: initial SvelteKit setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# RoundedChickens
+Team 1540's scouting app for the 2023 BunnyBots game, Rabbit Roundup!
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1 class="text-3xl text-amber-300 font-bold underline text-center p-4">
+  RoundedChickens
+</h1>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.slate.600);
+  }
+</style>
