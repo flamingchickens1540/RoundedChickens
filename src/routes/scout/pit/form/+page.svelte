785d5dc83e430e5a8ce1ac0d021ddf38bfe0ba9d
Elo Colburn <elocolburn@comcast.net>
2023-10-25 14:01:45 -0700

    feat: started pitscouting

---
+++ src/routes/scout/pit/form/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import { pit } from "../../../../lib/stores/stores";
+    import { PitData } from "../../../../lib/types";
+        
+</script>
+
+<h1>Pit Scouting - Form</h1>
+
