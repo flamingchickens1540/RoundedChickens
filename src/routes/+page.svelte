afa42234cec85b291b1cf1e015455f12acf0a25c
Blaze Annison <blaze@Blazes-MacBook-Air.local>
2023-10-30 16:12:55 -0700

    feat: moved work from +page.svelte to component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,48 +1,5 @@
-<h1 class="text-3xl text-amber-300 font-bold underline text-center p-4">
-  RoundedChickens
-</h1>
+<script>
+    import HybridLocation from "$lib/components/hybridLocation.svelte";
 
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.slate.600);
-  }
-  #body {
-    border: 1px solid black;
-    padding: 20px 50px 20px 50px;
-    text-align: center;
-  }
-  #heading {
-    color: crimson;
-    font-size: 50px;
-  }
-  #buttons {
-    border: 2px solid black;
-    display: flex;
-    flex-direction: column;
-  }
-  #far {
-    border: 1px solid black;
-    background-color: darkorange;
-    padding: 50px 1px 50px 1px;
-  }
-  #mid {
-    border: 1px solid black;
-    background-color: coral;
-    padding: 50px 50px 50px 50px;
-  }
-  #close {
-    border: 1px solid black;
-    background-color: darksalmon;
-    padding: 50px 50px 50px 50px;
-  }
-</style>
-
-<body id="body">
-  <h1 id="heading">1540A</h1>
-  <h1>Hybrid Location</h1>
-  <div id="buttons">
-    <button id="far">Far</button>
-    <button id="mid">Mid</button>
-    <button id="close">Close</button>
-  </div>
-</body>
+</script>
+<HybridLocation></HybridLocation>
