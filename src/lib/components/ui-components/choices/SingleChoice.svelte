7cb45a33f7abe1f3ca98223b1466217c4a964c49
BREgitLEEhub2222 <78449263+Brelee2222@users.noreply.github.com>
2023-11-03 18:03:30 -0700

    Added choices

---
+++ src/lib/components/ui-components/choices/SingleChoice.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    let choices : string[];
+    let selected : number;
+
+    function handleClick(index : number) {
+        selected = index;
+    }
+</script>
+
+{#each choices as choice, index}
+    <button on:click={() => handleClick(index)}>
+        <slot choice={choice} index={index}/>
+    </button>
+{/each}
