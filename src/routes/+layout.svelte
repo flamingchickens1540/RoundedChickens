209fd0fdaa94178cee9a844823c765ffc3043f50
= <=>
2023-11-15 16:49:49 -0800

    feat: supabase connection setup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,28 @@
   import "../app.css";
   import "@fontsource/poppins"
   import "@fontsource/teko"
+	import { invalidate } from '$app/navigation'
+	import { onMount } from 'svelte'
+
+	export let data
+
+	let { supabase, session } = data
+	$: ({ supabase, session } = data)
+
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((event, _session) => {
+			if (_session?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth')
+			}
+		})
+
+		return () => data.subscription.unsubscribe()
+	})
 </script>
 
-<slot />
+<div class="container" style="padding: 50px 0 100px 0">
+	<slot />
+</div>
 
 <style>
   :global(html) {
