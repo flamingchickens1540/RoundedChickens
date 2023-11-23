32dd67e62b9ad5260e98fbd0dde86e3f2e7cf38b
Elo Colburn <elocolburn@comcast.net>
2023-11-22 21:44:45 -0800

    feat: potential solution for production io

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from 'vite';
 import { ioServerPlugin } from "./webSocketPlugin.js"
 
 export default defineConfig({
+	build: {
+		rollupOptions: {
+
+		}
+	},
 	plugins: [sveltekit(), ioServerPlugin],
 	optimizeDeps: { exclude: ["fsevents"] },
 });
---
+++ webSocketPlugin.js
@@ -0,0 +1,53 @@
+// export const plugin = {
+//     name: 'sveltekit-socket-io',
+//     configureServer(server) {
+//         const io = new Server(server.httpServer);
+
+//         // Socket.io stuff goes here
+
+//         // This is located in the svelte config (see above "Socket.IO stuff goes here")
+//         io.on('connection', (socket) => {
+
+
+//             // Generate a random scout_id and send it to the client to display it
+//             // let scout_id = `scout ${Math.round(Math.random() * 999999)}`;
+
+//             // socket.to('scout').emit('scout_id', scout_id);
+//             // socket.to('admin').emit('')
+//             // Receive incoming messages and broadcast them
+//             // socket.on('message', (message) => {
+//             // 	io.emit('message', {
+//             // 		from: username,
+//             // 		message: message,
+//             // 		time: new Date().toLocaleString()
+//             // 	});
+//             // });
+//         });
+
+        
+
+//         console.log('SocketIO injected');
+//     }
+// }
+import { Server } from "socket.io"
+// import adapter from "svelte-adapter-bun";
+
+// export const server = {}
+
+// sjs might work
+
+// syntax ioServer = function (ctx) {
+// 	return #`const io = new Server(server.httpServer)
+// 				io.on('connect', (socket => {
+// 					socket.emit('eventFromServer', "Hello World")
+// 				}))`;
+//   }
+export const ioServerPlugin = {
+	name: "webSocketServer",
+	configureServer(server) {
+		const io = new Server(server.httpServer)
+		io.on('connect', (socket => {
+			socket.emit('eventFromServer', "Hello World")
+		}))
+	}
+}
