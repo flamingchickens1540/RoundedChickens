0cef9de079b4ed9d653e7a785e796002267a58fc
Elo Colburn <elocolburn@comcast.net>
2023-11-22 20:28:30 -0800

    feat: basic socket-io setup

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import adapter from "svelte-adapter-bun";
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import { Server } from "socket.io"
+import { ioServerPlugin } from "./webSocketPlugin.js"
 
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -8,43 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		vite: {
-			plugins: [
-				{
-					name: 'sveltekit-socket-io',
-					configureServer(server) {
-						const io = new Server(server.httpServer);
-
-						// Socket.io stuff goes here
-
-						// This is located in the svelte config (see above "Socket.IO stuff goes here")
-						io.on('connection', (socket) => {
-							// Generate a random scout_id and send it to the client to display it
-							let scout_id = `scout ${Math.round(Math.random() * 999999)}`;
-
-							socket.to('scout').emit('scout_id', scout_id);
-							socket.to('admin').emit('')
-							// Receive incoming messages and broadcast them
-							// socket.on('message', (message) => {
-							// 	io.emit('message', {
-							// 		from: username,
-							// 		message: message,
-							// 		time: new Date().toLocaleString()
-							// 	});
-							// });
-						});
-
-						
-
-						console.log('SocketIO injected');
-					}
-				}
-			]
-		}
 	}
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { ioServerPlugin } from "./webSocketPlugin.js"
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), ioServerPlugin],
 	optimizeDeps: { exclude: ["fsevents"] },
 });
