2b1f8d644b08d086d5d5c11af771d93ab8bb7c39
YayBlaze <blazeannison@icloud.com>
2023-11-28 16:26:50 -0800

    feat: changed colors hybrid location

--- src/routes/match/+page.svelte
+++ src/routes/match/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 
-<<<<<<< HEAD
     import HybridLocation from "$lib/components/HybridLocation.svelte";
     import HybridShots from "$lib/components/HybridShots.svelte";
     import Teleop from "$lib/components/Leaderboard.svelte"
@@ -8,13 +7,7 @@
 
 
 <!-- <Teleop/> -->
-<!-- <HybridLocation></HybridLocation> -->
+<HybridLocation></HybridLocation>
 <!-- <HybridShots></HybridShots> -->
 
 
-=======
-import Thanks from "$lib/components/Thanks.svelte"
-</script>
-
-<Thanks/>
->>>>>>> f6ba2158ea86986650dc2fe1e84d4c2ea9ba2a9d
